207f1298ff7bee865c472d2d557c93a985ed14ec
tinle <114992439+tinleviet@users.noreply.github.com>
2023-12-19 07:00:32 +0000

    Add is_in_list_naked_pairs function

--- src/naked_pairs.h
+++ src/naked_pairs.h
@@ -12,5 +12,6 @@ typedef struct{
     Cell *cell2;
 } NakedPair;
 
+bool is_in_list_naked_pairs(NakedPair *p_array, Cell *p);
 int naked_pairs(SudokuBoard *p_board);
 void find_naked_pairs(Cell **p_cells, int *p_counter, int *p_unset, UnsetInfo *p_unset_array, NakedPair *p_naked_pairs);
